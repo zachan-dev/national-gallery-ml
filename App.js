0653579bb253268300f6f48d538dab9d97c8ffde
Zach Chan <zach@zachan.dev>
2023-03-02 16:08:36 -0500

    change app.js

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Gyroscope } from 'expo-sensors';
+
+function round(num) {
+  return (Math.round(num * 1000) / 1000).toFixed(0);
+}
+
+const imageFolder = './assets/images/'; // The folder containing the images
+const imageArray = []; // The array to store the images
+
+const importAll = (r) => {
+  r.keys().forEach((key) => imageArray.push(r(key)));
+};
+
+try {
+  importAll(require.context(imageFolder, false, /\.(png|jpe?g|svg)$/));
+} catch (error) {
+  console.error(error);
+}
 
 export default function App() {
 
@@ -24,12 +41,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Gyroscope:</Text>
-      <Text style={styles.text}>x: {x}</Text>
-      <Text style={styles.text}>y: {y}</Text>
-      <Text style={styles.text}>z: {z}</Text>
+      <Text style={styles.text}>x: {round(x)}</Text>
+      <Text style={styles.text}>y: {round(y)}</Text>
+      <Text style={styles.text}>z: {round(z)}</Text>
       <Image
         style={styles.tinyLogo}
-        source={require('./assets/pack/0001.png')}
+        source={{uri: '0032.png'}}
       />
       <StatusBar style="auto" />
     </View>
