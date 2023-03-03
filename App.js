65bf016ece4b401088e6a96b1d8b94bcf4021e49
Zach Chan <zach@zachan.dev>
2023-03-02 20:03:09 -0500

    improve speed and quality

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Gyroscope } from 'expo-sensors';
 import images from './images';
+
+import { Asset } from 'expo-asset';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 function round(num) {
   return (Math.round(num * 1000) / 1000).toFixed(0);
 }
 
 function mapping(num) {
-
+  if (num < 0) return 0;
+  else if (num > 99) return 99;
+  else return num;
 }
 
 let imgNum = 50;
 
 export default function App() {
+
+  let [isLoaded, setIsLoaded] = useState(false);
+
+  let cacheResources = async () => {
+    const cacheImages = images.map(image => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    return Promise.all(cacheImages);
+  };
+
 
   const [{ x, y, z }, setData] = useState({
     x: 0,
@@ -25,22 +44,33 @@ export default function App() {
   const _fast = () => Gyroscope.setUpdateInterval(16);
 
   useEffect(() => {
+    const loadResources = async () => {
+      await cacheResources();
+      setIsLoaded(true);
+    }
+
+    loadResources();
+
     Gyroscope.addListener(gyroscopeData => {
       setData(gyroscopeData);
-      console.log(imgNum)
-      imgNum = Number(Number(imgNum) - Number(round(gyroscopeData.y)));
+      imgNum = mapping(Number(Number(imgNum) - Number(round(gyroscopeData.y))));
     });
     _fast();
     console.log("Subscribed to gyroscope");
   }, []);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isLoaded]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{imgNum}</Text>
-      <Text style={styles.text}>Gyroscope:</Text>
-      <Text style={styles.text}>x: {round(x)}</Text>
-      <Text style={styles.text}>y: {round(y)}</Text>
-      <Text style={styles.text}>z: {round(z)}</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Image
         style={{width: '100%', height: '50%'}}
         source={images[parseInt(imgNum)]}
